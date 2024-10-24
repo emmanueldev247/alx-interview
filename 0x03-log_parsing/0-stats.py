d86567ed469bc02445d3af3f5f5efa6d44ba73c4
Emmanuel Ademola <mailemmydee@gmail.com>
2024-10-24 18:33:15 +0000

    fixed: linter error

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -41,10 +41,10 @@ signal.signal(signal.SIGINT, handle_sigint)
 # Entry point
 # possible status code
 cache = {
-    '200':0, '301':0,
-    '400':0, '401':0,
-    '403':0, '404':0,
-    '405':0, '500':0
+    '200': 0, '301': 0,
+    '400': 0, '401': 0,
+    '403': 0, '404': 0,
+    '405': 0, '500': 0
 }
 
 total_size = 0
