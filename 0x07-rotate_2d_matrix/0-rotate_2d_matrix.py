2a5406b77a07bd183ac35cec86330e9afa45d2b2
Emmanuel Ademola <mailemmydee@gmail.com>
2024-11-25 12:24:33 +0000

    fixed: Transpose error

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -12,7 +12,7 @@ def rotate_2d_matrix(matrix):
     # First transpose
     n = len(matrix)
     for i in range(n):
-        for j in range(1, n):
+        for j in range(i+1, n):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
 
     # Then reverse
