825d206f3bac07fde9e06dbcf2bcc636b7a326c8
Emmanuel Ademola <mailemmydee@gmail.com>
2024-12-03 05:55:40 +0000

    fixed: pycodestyle errors

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,31 +1,38 @@
 #!/usr/bin/python3
+"""Island Perimeter"""
+
 
 def island_perimeter(grid):
-  rows = len(grid)
-  cols = len(grid[0])
+    """Function that returns the perimeter of the island described in grid
+    Args:
+        grid (list):  is a list of list of integers
+    Returns:
+        the perimeter
+    """
+    rows = len(grid)
+    cols = len(grid[0])
 
-  sum_per = 0
+    sum_per = 0
 
-
-  for row in range(rows):
-    for col in range(cols):
-      if grid[row][col] == 1:
-        per = 4
-        """Check up"""
-        if row > 0:
-          if grid[row - 1][col] == 1:
-            per -= 1
-        """Check down"""
-        if row < rows - 1:
-          if grid[row + 1][col] == 1:
-            per -= 1
-        """Check left"""
-        if col > 0:
-          if grid[row][col - 1] == 1:
-            per -= 1
-        """Check right"""
-        if col < cols - 1:
-          if grid[row][col + 1] == 1:
-            per -= 1
-        sum_per += per
-  return sum_per
+    for row in range(rows):
+        for col in range(cols):
+            if grid[row][col] == 1:
+                per = 4
+                """Check up"""
+                if row > 0:
+                    if grid[row - 1][col] == 1:
+                        per -= 1
+                """Check down"""
+                if row < rows - 1:
+                    if grid[row + 1][col] == 1:
+                        per -= 1
+                """Check left"""
+                if col > 0:
+                    if grid[row][col - 1] == 1:
+                        per -= 1
+                """Check right"""
+                if col < cols - 1:
+                    if grid[row][col + 1] == 1:
+                        per -= 1
+                sum_per += per
+    return sum_per
