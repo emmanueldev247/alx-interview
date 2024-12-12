31682a2f8f670a8f35dfd2cb6b0dc679e65593f2
Emmanuel Ademola <mailemmydee@gmail.com>
2024-12-12 18:58:06 +0000

    added: main_0 file

--- 0x0A-primegame/main_0.py
+++ 0x0A-primegame/main_0.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 
-isWinner = __import__('0-prime_game')
+isWinner = __import__('0-prime_game').isWinner
+
+
+print("Winner: {}".format(isWinner(3, [4, 5, 1])))
+print("Winner: {}".format(isWinner(5, [2, 5, 1, 4, 4, 3])))
