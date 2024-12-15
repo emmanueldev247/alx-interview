62a37d3e4294ea90ffbac0546776503b08f9e5a2
Emmanuel Ademola <mailemmydee@gmail.com>
2024-12-15 07:19:58 +0000

    updated: optimized logic

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -18,62 +18,30 @@ def isWinner(x, nums):
     if not nums or x < 1:
         return None
 
+    max_num = max(nums)
+    primes = sieve_of_eratosthenes(max_num)
+    prime_count = [0] * (max_num + 1)
+
+    for i in range(1, max_num + 1):
+        prime_count[i] = prime_count[i - 1]
+        if primes[i]:
+            for j in range(i, max_num + 1, i):
+                prime_count[j] += 1
+
     players = {'Maria': 0, 'Ben': 0}
-    win = {}
 
-    for i in range(x):
-        limit = nums[i]
-        if limit in win:
-            if win[limit] == 'Ben':
-                players['Ben'] += 1
-            else:
-                players['Maria'] += 1
-            continue
-
-        primes = sieve_of_eratosthenes(limit)
-        full_nums = [j for j in range(1, nums[i] + 1)]
-
-        firstPlayer = True
-        while has_prime(full_nums):
-            firstPlayer = not firstPlayer
-            if firstPlayer:
-                current_player = 'Maria'
-            else:
-                current_player = 'Ben'
-
-            for prime in primes:
-                if prime in full_nums:
-                    full_nums = [num for num in full_nums if num % prime != 0]
-                    break
-
-        if firstPlayer:
-            win[limit] = 'Ben'
+    for n in nums:
+        if prime_count[n] % 2 == 0:
             players['Ben'] += 1
         else:
-            win[limit] = 'Maria'
             players['Maria'] += 1
-    winner = max(players, key=players.get)
-    return winner
 
-
-def has_prime(lst):
-    """Checks if a list contains a prime number.
-
-    Args:
-        lst: A list of integers.
-
-    Returns:
-        True if the list contains a prime number, False otherwise.
-    """
-    for n in lst:
-        if n <= 1:
-            continue
-        for i in range(2, int(sqrt(n)) + 1):
-            if n % i == 0:
-                break
-        else:
-            return True
-    return False
+    if players['Maria'] > players['Ben']:
+        return "Maria"
+    elif players['Ben'] > players['Maria']:
+        return "Ben"
+    else:
+        return None
 
 
 def sieve_of_eratosthenes(limit):
@@ -86,31 +54,10 @@ def sieve_of_eratosthenes(limit):
     Returns:
         A list of prime numbers up to the given limit.
     """
-    if limit < 2:
-        return []
     primes = [True] * (limit + 1)
     primes[0] = primes[1] = False
-    for i in range(2, int(sqrt(limit)) + 1):
+    for i in range(2, int(limit**0.5) + 1):
         if primes[i]:
             for j in range(i * i, limit + 1, i):
                 primes[j] = False
-    return [i for i in range(2, limit + 1) if primes[i]]
-
-
-def sqrt(x):
-    """Calculates the square root of a number using the Babylonian method.
-
-    Args:
-        x: The number to find the square root of.
-
-    Returns:
-        The square root of x as a float.
-    """
-    if x == 0:
-        return 0
-
-    guess = x / 2
-    while abs(guess**2 - x) > 0.00001:
-        guess = (guess + x / guess) / 2
-
-    return guess
+    return primes
